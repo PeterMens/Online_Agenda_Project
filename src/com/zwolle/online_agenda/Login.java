0d48499420dd58bca6bab7661ca6b583a25d643a
Student <Student>
2015-11-17 17:13:52 +0100

    register jsp

--- src/com/zwolle/online_agenda/Login.java
+++ src/com/zwolle/online_agenda/Login.java
@@ -3,6 +3,6 @@ package com.zwolle.online_agenda;
 public class Login {
 
 	
-	
+	// check form info uit jsp met database, als het klopt, haal calendar uit database en toon /calendar jsp.
 	
 }
---
+++ src/com/zwolle/online_agenda/Register.java
@@ -0,0 +1,7 @@
+package com.zwolle.online_agenda;
+
+public class Register {
+// register new user with name and password
+// generate database for user 
+// koppel database met username+password
+}
